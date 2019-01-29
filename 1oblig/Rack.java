96e2a5d7775797a8100cccced3a9b73663594b57
georgmagne <georgmagne@gmail.com>
2019-01-30 00:55:59 +0100

    Mindre endringer på rack og regneklynge. Må fikse cannot find symbol på rack.settInn()

--- 1oblig/Rack.java
+++ 1oblig/Rack.java
@@ -7,32 +7,33 @@ public class Rack {
     int maxNoder = maxNoderPerRack;
   }
 
-  public void settInnNode(int giMinne, int giPros){
+  public void settInn(Node node){
       for (int i=0; i < nodeListe.length; i++){ // Itererer gjennom nodeListe
-          if (nodeListe[i] == null) {
-            nodeListe[i] = new Node(giMinne, giPros);
+          if (nodeListe[i] == null) { // Hvis plassen i arrayet er ledig.
+            nodeListe[i] = node; // Setter inn ny node på den første ledige plassen i racket.
             break; // Ut av for-løkka.
           }
       }
   }
 
   public boolean erFull(){
-    for (int i=0; i < nodeListe.length; i++){
-      if (nodeListe[i] == null){
-        return false;
+    for (int i=0; i < nodeListe.length; i++){ // Itererer gjennom arrayet med noder
+      if (nodeListe[i] == null){ // Hvis plassen er ledig.
+        return false; // Racket er ikke fullt
       }
     }
-    return true;
+    return true; // Racket er fullt.
   }
-  public int noderMedNokMinne(int nokMinne){
-    int antall = 0;
 
-    for (int i=0; i < nodeListe.length; i++){
-      if (nodeListe[i].nokMinne(nokMinne)){
-        antall += 1;
+  public int noderMedNokMinne(int nokMinne){
+    int antall = 0; // Teller for antall noder, som oppfyller kravene.
+
+    for (int i=0; i < nodeListe.length; i++){ // Interer gjennom arrayet
+      if (nodeListe[i].nokMinne(nokMinne)){  // Sjekker om noden har nok minne, sammenlign med parameteret.
+        antall += 1; // Hvis ja
       }
     }
-    return antall;
+    return antall; // Etter at alle nodene  er sjekket.
   }
 
   /*  public boolean erTom(Node[] nodeListe){
--- 1oblig/Regneklynge.java
+++ 1oblig/Regneklynge.java
@@ -5,7 +5,8 @@ class Regneklynge {
     public int maxNoderPerRack = 12; // Endre senere; lest fra fil.
     public int giMinne = 32; // Endre; lese fra fil
     public int giPros = 1;
-    public ArrayList<Rack> rackListe;  // Bruker ArrayList fordi antall rack ikke er fastsatt, og potensielt uendelig.
+    public ArrayList<Rack> rackListe = new ArrayList<Rack>();  // Bruker ArrayList fordi antall rack ikke er fastsatt, og potensielt uendelig.
+    public Node node = new Node(giMinne, giPros);
     /* metoder til arraylist
     add()
       rackListe.add(new Rack(asdasdasd,asdasdasd))
@@ -15,7 +16,7 @@ class Regneklynge {
     */
     public Regneklynge(){ // Kontruktør
       rackListe = new ArrayList<Rack>(); // Setter rackList lik nytt ArrayList med Rack-objekter.
-      rackListe.add(new Rack(maxNoderPerRack)); // Ett rack blir opprettet samtidig som en regneklyngen.
+      rackListe.add(new Rack(maxNoderPerRack)); // Ett rack blir opprettet samtidig som regneklyngen.
     }
 
     public void lagRack(){
@@ -24,16 +25,16 @@ class Regneklynge {
 
     public void settInnNode(){ // Node opprettes og settes inn i passende rack i regneklyngen.
       if (!rackListe.get(rackListe.size()-1).erFull()) { // Hvis det siste racket ikke er fullt.
-        rackListe.settInnNode(giMinne, giPros); // Node blir satt inn.
+        rackListe.settInn(node); // Node blir satt inn.
       } else {
         lagRack(); // Nytt rack blir opprettet
-        rackListe.settInnNode(giMinne, giPros); // Node blir satt inn
+        rackListe.settInn(node); // Node blir satt inn
       }
     }
 
     public int noderMedNokMinne(int nokMinne){ // Returnerer antall noder som har nok minne, sammenlignet med det gitte parameteret.
       int totNoderNokMinne = 0;
-      for (Rack elem: rackList){
+      for (Rack elem: rackListe){
         totNoderNokMinne += elem.noderMedNokMinne(nokMinne);
       }
 
@@ -41,7 +42,7 @@ class Regneklynge {
 
     public int antProsessorer(){ // Returnerer antall prosessorer i regneklyngen.
       int totPros = 0; // Prosessor teller.
-      for (Rack elem: rackList){ // Hvert rack i rackList.
+      for (Rack elem: rackListe){ // Hvert rack i rackList.
         for (Node n: elem){ // Hver node i rack.
           totPros += n.getPros(); // Legger til Noden sitt antall prosessorer.
         }
