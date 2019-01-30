df97d93d21ecf2be2a2fd476b48491d6ef7b4773
georgmagne <georgmagne@gmail.com>
2019-01-30 12:03:17 +0100

    Fikset cannot find symbol på rack.settInn()

--- 1oblig/Regneklynge.java
+++ 1oblig/Regneklynge.java
@@ -6,7 +6,7 @@ class Regneklynge {
     public int giMinne = 32; // Endre; lese fra fil
     public int giPros = 1;
     public ArrayList<Rack> rackListe = new ArrayList<Rack>();  // Bruker ArrayList fordi antall rack ikke er fastsatt, og potensielt uendelig.
-    public Node node = new Node(giMinne, giPros);
+    public Node node = new Node(giMinne, giPros); // Må endres; kun for test.
     /* metoder til arraylist
     add()
       rackListe.add(new Rack(asdasdasd,asdasdasd))
@@ -15,20 +15,20 @@ class Regneklynge {
     size()
     */
     public Regneklynge(){ // Kontruktør
-      rackListe = new ArrayList<Rack>(); // Setter rackList lik nytt ArrayList med Rack-objekter.
+      rackListe = new ArrayList<Rack>(); // Setter rackListe lik nytt ArrayList med Rack-objekter.
       rackListe.add(new Rack(maxNoderPerRack)); // Ett rack blir opprettet samtidig som regneklyngen.
     }
 
     public void lagRack(){
-      rackListe.add(new Rack(maxNoderPerRack)); // Oppretter nytt tomt rack og legger det til rackList.
+      rackListe.add(new Rack(maxNoderPerRack)); // Oppretter nytt tomt rack og legger det til rackListe.
     }
 
     public void settInnNode(){ // Node opprettes og settes inn i passende rack i regneklyngen.
       if (!rackListe.get(rackListe.size()-1).erFull()) { // Hvis det siste racket ikke er fullt.
-        rackListe.settInn(node); // Node blir satt inn.
-      } else {
+        rackListe.get(rackListe.size()-1).settInn(node); // Node blir satt inn i siste element i rackListe
+      } else { // Hvis alle rack i rackListe er fulle.
         lagRack(); // Nytt rack blir opprettet
-        rackListe.settInn(node); // Node blir satt inn
+        rackListe.get(rackListe.size()-1).settInn(node); // Node blir satt inn i siste element i rackListe
       }
     }
 
@@ -42,7 +42,7 @@ class Regneklynge {
 
     public int antProsessorer(){ // Returnerer antall prosessorer i regneklyngen.
       int totPros = 0; // Prosessor teller.
-      for (Rack elem: rackListe){ // Hvert rack i rackList.
+      for (Rack elem: rackListe){ // Hvert rack i rackListe.
         for (Node n: elem){ // Hver node i rack.
           totPros += n.getPros(); // Legger til Noden sitt antall prosessorer.
         }
@@ -50,6 +50,6 @@ class Regneklynge {
   }
 
     public int getAntRack(){
-      return rackListe.size(); // Antall rack i Regneklynge.
+      return rackliste.size(); // Antall rack i Regneklynge.
     }
 }
