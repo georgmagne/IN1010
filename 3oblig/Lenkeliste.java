c50ac4e3d5ac788406074f7a850cecbcbdb6878b
Georg Magneshaugen <georgmag@sanderstead.ifi.uio.no>
2019-03-01 13:20:14 +0100

    Start oblig 3.

---
+++ 3oblig/Lenkeliste.java
@@ -0,0 +1,32 @@
+public class Lenkeliste<T> implements Liste<T>{
+  
+  public int stoerrelse() {
+
+  }
+
+  public void leggTil(int pos, T x) {
+
+  }
+
+  public void legTil(T x) {
+
+  }
+
+  public void sett(int pos, T x) {
+
+  }
+
+  public T hent (int pos) {
+
+  }
+
+  public T fjern (int pos) {
+
+  }
+
+  public T fjern() {
+
+  }
+
+
+}
